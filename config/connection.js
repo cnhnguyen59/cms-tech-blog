c11f074d344e1b596836c0e52b6f51336f966922
Christine Nguyen <cnhnguyen59@gmail.com>
2022-03-08 21:18:42 -0500

    deploying to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,23 @@
 const Sequelize = require('sequelize');
 require('dotenv').config()
 
+let sequelize;
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: 'localhost',
-        dialect:'mysql',
-        port:3306,
-    }
-)
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+    console.log("Connected to JAWS");
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: "localhost",
+            dialect: "mysql",
+            port: 3306,
+        }
+    );
+    console.log("Connected to localhost");
+}
 
 module.exports = sequelize;
